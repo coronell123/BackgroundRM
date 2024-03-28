5c22ddb35fafdd267afa098fa83d1bbcf3b6f41a
coronell123 <66991556+coronell123@users.noreply.github.com>
2024-03-28 16:22:57 +0100

    Update image2image.py

--- build/COPY_ROOT_EXTRA/opt/serverless/handlers/image2image.py
+++ build/COPY_ROOT_EXTRA/opt/serverless/handlers/image2image.py
@@ -13,7 +13,7 @@ downloading any URL's to the input directory and replacing the URL with a local 
 
 class Image2Image(BaseHandler):
     
-    WORKFLOW_JSON = "/opt/serverless/workflows/image2image.json"
+    WORKFLOW_JSON = "/opt/serverless/workflows/RemoveBG.json"
     
     def __init__(self, payload):
         super().__init__(payload, self.WORKFLOW_JSON)
@@ -22,35 +22,13 @@ class Image2Image(BaseHandler):
 
     def apply_modifiers(self):
         timestr = time.strftime("%Y%m%d-%H%M%S")
-        self.prompt["prompt"]["3"]["inputs"]["seed"] = self.get_value(
-            "seed",
-            random.randint(0,2**32))
-        self.prompt["prompt"]["3"]["inputs"]["steps"] = self.get_value(
-            "steps",
-            20)
-        self.prompt["prompt"]["3"]["inputs"]["sampler_name"] = self.get_value(
-            "sampler_name",
-            "dpmpp_2m")
-        self.prompt["prompt"]["3"]["inputs"]["scheduler"] = self.get_value(
-            "scheduler",
-            "normal")
-        self.prompt["prompt"]["3"]["inputs"]["denoise"] = self.get_value(
-            "denoise",
-            0.8700000000000001)
-        self.prompt["prompt"]["6"]["inputs"]["text"] = self.get_value(
-            "include_text",
-            "")
-        self.prompt["prompt"]["7"]["inputs"]["text"] = self.get_value(
-            "exclude_text",
-            "")
-        self.prompt["prompt"]["10"]["inputs"]["image"] = self.get_value(
-            "input_image",
-            "")
-        self.prompt["prompt"]["14"]["inputs"]["ckpt_name"] = self.get_value(
-            "ckpt_name",
-            "v1-5-pruned-emaonly.ckpt")
-        
-        
+        """
+        Input node
+        """
+        self.prompt["prompt"]["15"]["inputs"]["image"] = self.get_value(
+            "input_image","")
+        """
+
 """
 Example Request Body:
 
